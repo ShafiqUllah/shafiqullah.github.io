6bd56492e56910446fc9c4df8499aa5e3d44c0d7
Md Shafiq Ullah <shohagbit0404@gmail.com>
2022-11-08 15:28:24 -0600

    WAP lab assignment 5
    Signed-off-by: Md Shafiq Ullah <shohagbit0404@gmail.com>

--- docs/js/assignment5.js
+++ docs/js/assignment5.js
@@ -13,119 +13,117 @@ function maxOfThree(a, b, c) {
   else if (b > a && b > c) return b;
   else return c;
 }
-console.log(maxOfThree(5,19,10));
+console.log(maxOfThree(5, 19, 10));
 
 //3
-function isVowel(character){    
-    const vowel = ["a","e","i","o","u","A","E","I","O","U"];   
-    return vowel.find(i=>i==character) ? true : false; 
+function isVowel(ch) {
+  const vowel = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+  return vowel.find((i) => i == ch) ? true : false;
 }
-console.log("b is vowel - " +  isVowel('b'));
+console.log("b is vowel - " + isVowel("ba"));
 
-// 4 
-function sum(arr){
-    return arr.reduce((sum,i)=>sum+i,0);
+// 4
+function sum(arr) {
+  let a = 0;
+  for (let i = 0; i < arr.length; i++) {
+    a += arr[i];
+  }
+  return a;
 }
-console.log("sum([1, 2, 3, 4]) is " + sum([1, 2, 3, 4]) );
+console.log("sum([1, 2, 3, 4]) is " + sum([1, 2, 3, 4]));
 
-function multiply(arr){
-    return arr.reduce((multiply,i)=>multiply*i,1);
+function multiply(arr) {
+  let a = 1;
+  for (let i = 0; i < arr.length; i++) {
+    a *= arr[i];
+  }
+  return a;
 }
-console.log("multiply([1, 2, 3, 4]) is " +  multiply([1, 2, 3, 4]));
+console.log("multiply([1, 2, 3, 4]) is " + multiply([1, 2, 3, 4]));
 
-// 5 
-function reverse(str){    
-     return str.split("").reverse().join("");
+// 5
+function reverse(str) {
+  return str.split("").reverse().join("");
 }
-console.log("Input is Hello World , output is " + reverse('Hello World'));
+console.log("Input is Hello World , output is " + reverse("Hello World"));
 
-// 6 
-function findLongestWord(words){
-    let max=0;
-    for(let i=0; i<words.length; i++){
-        if(words[i].length>max) 
-            max = words[i].length;
-    }
-    return max;
+// 6
+function findLongestWord(words) {
+  let max = 0;
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > max) max = words[i].length;
+  }
+  return max;
 }
-console.log("findLongestWord(['Hello', 'World', 'Md']) is  " + findLongestWord(['Hello', 'World', 'Md']) );
+console.log(
+  "findLongestWord(['Hello', 'World', 'Md']) is  " +
+    findLongestWord(["Hello", "World", "Md"])
+);
 
-// 7 
-function filterLongWords(words,i){   
-    return words.filter(x=>x.length>i);    
+// 7
+function filterLongWords(words, i) {
+  return words.filter((x) => x.length > i);
 }
-console.log("filterLongWords(['Hello', 'World', 'Md'], 2) is  " +  filterLongWords(['Hello', 'World', 'Md'], 2) );
+console.log(
+  "filterLongWords(['Hello', 'World', 'So'], 3) is  " +
+    filterLongWords(["Hello", "World", "So"], 3)
+);
 
 //8
-function computeSumOfSquares(a){
-    const f = a.reduce(function(prevVal, elem, i, array){
-        return parseInt(prevVal) + parseInt(elem*elem);},0); 
-    return f;
+function computeSumOfSquares(a) {
+  return a.reduce((x, y) => x + y * y, 0);
 }
-console.log("8 -> "+ computeSumOfSquares([1,2,3]));
+console.log("8 -> " + computeSumOfSquares([1, 2, 3]));
 
 //9
-function printOddNumbersOnly(a){
-    const c = a.filter(function(elem, i, array){
-        return elem % 2 !== 0;});
-        console.log(c);
-    return c;
+function printOddNumbersOnly(a) {
+  return a.filter((x) => parseInt(x) % 2 != 0);
 }
 
-console.log("9 -> ");
-printOddNumbersOnly([1,2,3,4,5]);
+console.log("9 -> " + printOddNumbersOnly([1, 2, 3, 4, 5]));
 
 //10
-function computeSumOfSquaresOfEvensOnly(a){
-    const c = a.filter(function(elem, i, array){
-        return parseInt(elem) % 2 == 0;})
-        .reduce(function(prevVal, elem2, j, array){
-        return parseInt(prevVal) + parseInt(elem2)*parseInt(elem2);},0)
-    return c;
+function computeSumOfSquaresOfEvensOnly(a) {
+  return a.filter((x) => x % 2 == 0).reduce((a, b) => a + b * b, 0);
 }
-console.log("10 -> "+ computeSumOfSquaresOfEvensOnly([1,2,3,4,5]));
+console.log("10 -> " + computeSumOfSquaresOfEvensOnly([1, 2, 3, 4, 5]));
 
 //11
-function sum2(a){
-    const f = a.reduce(function(prevVal, elem, i, array){
-        return parseInt(prevVal) + parseInt(elem);}); 
-    return f;
+function sum2(arr) {
+  return arr.reduce((a, b) => a + b, 0);
 }
-function multiply2(a){
-    const f = a.reduce(function(prevVal, elem, i, array){
-        return parseInt(prevVal) * parseInt(elem);}); 
-    return f;
+console.log("sum([1, 2, 3, 4]) is " + sum2([1, 2, 3, 4]));
+
+function multiply2(arr) {
+  return arr.reduce((a, b) => a * b, 1);
 }
+console.log("multiply([1, 2, 3, 4]) is " + multiply2([1, 2, 3, 4]));
 
 //12
-function findSecondBiggest(a){
+function findSecondBiggest(a) {
     let max = a[0];
     let second = 0;    
     for (var i = 0, n = a.length; i < n; ++i) {
-        var nr = +a[i]; // convert to number first    
+        var nr = a[i];    
         if (nr > max) {
-            second = max; // save previous biggest value
+            second = max; 
             max = nr;
         } else if (nr < max && nr > second) {
-            second = nr; // new second biggest value
+            second = nr; 
         }
     }
     return second;
 }
 
-console.log("12 -> "+ findSecondBiggest([19,9,11,0,12]))
+console.log("12 -> " + findSecondBiggest([19, 9, 11, 0, 12]));
 
-//13
-function printFibo(n,a,b){
-    let arr = [a, b];    
-    if(n==1) return arr = [a];
-    
-    for (let i = 2; i < n; i++){
-        arr.push(arr[i - 2] +  arr[i -1]);        
-    }
-    console.log("Fibonacci sequence: ");
-    console.log(arr);
-    return arr.join(',');
+13
+function printFibo(n, a, b) {
+  let arr = [a, b];
+  for (let i = 2; i < n; i++) {
+    arr.push(arr[i - 2] + arr[i - 1]);
+  }
+  return arr.join(",");
 }
 
-console.log("13 -> "+ printFibo(10,0,1))
+console.log("13 (Fibonacci sequence: )-> " + printFibo(10, 0, 1));
