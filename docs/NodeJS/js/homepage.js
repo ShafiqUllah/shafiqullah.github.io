552d0c6d8deadb96c5bd0a07f9683c68bd961b94
Md Shafiq Ullah <shohagbit0404@gmail.com>
2022-11-19 23:52:03 -0600

    Lab assignment 11 nodejs intor
    Signed-off-by: Md Shafiq Ullah <shohagbit0404@gmail.com>

---
+++ docs/NodeJS/js/homepage.js
@@ -0,0 +1,15 @@
+"use strict";
+
+window.onload = () => {
+    // const myForm = document.getElementById("myform")
+    // myForm.onsubmit = (event) => {
+    //     event.preventDefault();
+
+    //     const firstNum = document.getElementById("firstNum");
+    //     const secondNum = document.getElementById("secondNum");
+
+    //     // alert("firstNum.value" );
+    //     return true;
+
+    // }
+}
